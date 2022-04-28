3a0fa5777bc868ab69e84a0749da2f62c70aa0e1
harshada <harshadasawant22@gmail.com>
2022-04-28 17:07:23 +0530

    Comparable and comparator

--- src/com/company/ArrayListExample.java
+++ src/com/company/ArrayListExample.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -60,15 +61,44 @@ public class ArrayListExample {
               System.out.println(p.getWeight());
 
           }
-
-
-
-
       }
+
+    static void StudentList(){
+        List<Student1> li = new ArrayList<Student1>();
+        li.add(new Student1(3,23,"harshada"));
+        li.add(new Student1(2,34,"Teju"));
+        li.add(new Student1(1,20,"Daksh"));
+        Collections.sort(li);
+        for(Student1 st : li){
+            System.out.println(st);
+
+        }
+    }
+
+    static void StudentSortList(){
+        List<Student2> li = new ArrayList<Student2>();
+        li.add(new Student2(3,23,"Harshada"));
+        li.add(new Student2(2,17,"Teju"));
+        li.add(new Student2(1,20,"Daksh"));
+        System.out.println("===============sort on Name ===============");
+        Collections.sort(li,new NameComparator());
+        for(Student2 st : li){
+            System.out.println(st);
+        }
+        System.out.println("===============sort on Age ===============");
+        Collections.sort(li,new AgeComparator());
+        for(Student2 st : li){
+            System.out.println(st);
+        }
+    }
+
     public static void main(String[] args) {
 //        arrayFunction();
 //        LinkedListFunction();
         PersonList();
+        StudentList();
+        StudentSortList();
+
     }
 
 }
